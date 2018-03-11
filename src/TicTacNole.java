142284ab630d9d4062043a81886571037207f006
Andrew Wood <wood.andrew9@gmail.com>
2018-03-10 21:22:14 -0500

    Fully working copy.

--- src/TicTacNole.java
+++ src/TicTacNole.java
@@ -17,7 +17,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-public class TicTacNole extends JFrame implements ActionListener
+public class TicTacNole extends JFrame
 {
     private final JLabel titleBar;
     private final JPanel titleArea;
@@ -25,8 +25,10 @@ public class TicTacNole extends JFrame implements ActionListener
     private final JPanel statusArea;
     private final JButton[] buttons;
     private final JLabel statusBar;
+    private final JLabel infoBar;
     private final JButton reset;
-    private static String[] buttonLabels = {" "," "," "," "," "," "," "," "," "};
+    private final static String[] buttonLabels = {" "," "," "," "," "," "," "," "," "};
+    private boolean xGoes = true; //starts out as X turn always
 
     //TicTacNole Constructor
     public TicTacNole()
@@ -44,39 +46,179 @@ public class TicTacNole extends JFrame implements ActionListener
         buttonArea = new JPanel();
         buttonArea.setLayout(new GridLayout(3,3)); //3 rows by 3 columns
         buttons = new JButton[buttonLabels.length];
+        Font defaultFont = new Font(Font.SANS_SERIF,Font.BOLD,26);
+
+        //Create handler object
+        TicTacNoleHandler handler = new TicTacNoleHandler();
 
         for (int count = 0; count < buttonLabels.length; ++count)
         {
             buttons[count] = new JButton(buttonLabels[count]);
-            buttons[count].addActionListener(this);
+            buttons[count].addActionListener(handler);
+            buttons[count].setFont(defaultFont);
             buttonArea.add(buttons[count]);
         }
 
         //Panel for Status Area - This will display current turn information as well as a reset button
         statusArea = new JPanel();
-        statusArea.setLayout(new GridLayout(2,1)); //2 rows by 1 column
-        statusBar = new JLabel("Dummy Text for Now");
+        statusArea.setLayout(new GridLayout(3,1)); //3 rows by 1 column
+        statusBar = new JLabel("X's Turn"); //default starting is X's turn
         statusBar.setHorizontalAlignment(JLabel.CENTER);
+        infoBar = new JLabel("Created by Andrew Wood");
+        infoBar.setHorizontalAlignment(JLabel.CENTER);
+
         reset = new JButton("Reset Game");
+        reset.addActionListener(handler);
         statusArea.add(statusBar);
         statusArea.add(reset);
-
+        statusArea.add(infoBar);
 
         //Add all panels to the Frame
         add(titleArea,BorderLayout.NORTH);
         add(buttonArea,BorderLayout.CENTER);
         add(statusArea,BorderLayout.SOUTH);
-        this.pack();
-
-
     }
 
-    @Override
-    public void actionPerformed(ActionEvent event)
+    private class TicTacNoleHandler implements ActionListener
     {
-        //put code here
+        @Override
+        public void actionPerformed(ActionEvent event)
+        {
+            //Determine which button was pressed
+            Object sourceButton = event.getSource();
+
+            //if the reset button was pressed
+            if (sourceButton == reset)
+            {
+                TicTacNole.this.resetBoard(); //reset the board
+            }
+            else //a board button was clicked
+            {
+                //loop through the buttons until source of click is found
+                for (JButton button : buttons) {
+                    if (button == sourceButton) {
+                        if (button.getText().equals(" ")) //if the space is empty
+                        {
+                            if (xGoes) {
+                                button.setText("X");
+                                statusBar.setText("O's Turn");
+                            } else {
+                                button.setText("O");
+                                statusBar.setText("X's Turn");
+                            }
+                            xGoes = !xGoes;
+                        }
+                    }
+                }
+                //now check to see if the game is in an end state
+                GameState state = checkEnd();
+                int playAgain = 0;
+                switch(state)
+                {
+                    case CONTINUE:
+                        break; //do nothing
+                    case DRAW:
+                        playAgain = JOptionPane.showConfirmDialog(TicTacNole.this,"It's a draw! " +
+                                "Would you like to play again?","Draw",JOptionPane.YES_NO_OPTION,
+                                JOptionPane.QUESTION_MESSAGE);
+                        if (playAgain == JOptionPane.YES_OPTION)
+                        {
+                            resetBoard();
+                            xGoes = true;
+                        }
+                        else
+                        {
+                            System.exit(0);
+                        }
+                        break;
+                    case X_WIN:
+                        playAgain = JOptionPane.showConfirmDialog(TicTacNole.this,"X Wins! " +
+                                        "Would you like to play again?","X Wins",JOptionPane.YES_NO_OPTION,
+                                JOptionPane.QUESTION_MESSAGE);
+                        if (playAgain == JOptionPane.YES_OPTION)
+                        {
+                            resetBoard();
+                            statusBar.setText("X's Turn");
+                            xGoes = true;
+                        }
+                        else
+                        {
+                            System.exit(0);
+                        }
+                        break;
+                    case O_WIN:
+                        playAgain = JOptionPane.showConfirmDialog(TicTacNole.this,"O Wins! " +
+                                    "Would you like to play again?","O Wins",JOptionPane.YES_NO_OPTION,
+                            JOptionPane.QUESTION_MESSAGE);
+                        if (playAgain == JOptionPane.YES_OPTION)
+                        {
+                            resetBoard();
+                            xGoes = true;
+                        }
+                        else
+                        {
+                            System.exit(0);
+                        }
+                        break;
+                }
+
+            }
+        }
     }
 
+    //reset the board and start game over
+    private void resetBoard()
+    {
+        for (JButton button : buttons)
+        {
+            button.setText(" ");
+        }
+        xGoes = true;
+    }
+
+    private enum GameState {X_WIN,O_WIN,DRAW,CONTINUE}
+
+    //check to see if there is a winner of if the board is a draw
+    private GameState checkEnd()
+    {
+        final int [][] winningCombos = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+        //loop through arrays to see if end game is found
+        for (int [] winMatrix : winningCombos)
+        {
+                if (buttons[winMatrix[0]].getText().equals(buttons[winMatrix[1]].getText()) &&
+                        buttons[winMatrix[1]].getText().equals(buttons[winMatrix[2]].getText()) &&
+                        !buttons[winMatrix[0]].getText().equals(" "))
+                {
+                    //a win was found, process appropriately
+                    char winningLetter = buttons[winMatrix[0]].getText().charAt(0);
+                    switch (winningLetter) {
+                        case 'X':
+                            statusBar.setText("X Wins!");
+                            return GameState.X_WIN;
+                        case 'O':
+                            statusBar.setText("O Wins!");
+                            return GameState.O_WIN;
+                        default: //should never reach here
+                            System.err.println("Fatal error encountered.");
+                            System.exit(1);
+                    }
+                }
+        }
+        //No win was found, check to see if the board is full
+        boolean isFull = true;
+        for (JButton button : buttons)
+        {
+            if (button.getText().equals(" ")) {
+                isFull = false;
+                break;
+            }
+        }
+        if (isFull)
+            return GameState.DRAW;
+        else
+            return GameState.CONTINUE;
+    }
 
     //Tic Tac Nole's main class
     public static void main(String[] args)
@@ -85,113 +227,6 @@ public class TicTacNole extends JFrame implements ActionListener
         game.setVisible(true);
         game.setSize(360,360);
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
     }
 
 }
-
-    /*
-    private static int[][] winCombinations = new int[][]{
-            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //horizontal wins
-            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //vertical wins
-            {0, 4, 8}, {2, 4, 6}             //diagonal wins
-    };
-
-    private static JButton buttons[] = new JButton[9]; //create 9 buttons
-
-    public static void main(String[] args) {
-        gamePanel(); //launch game
-    }
-
-
-
-    private static void gamePanel() {
-        JFrame frame = new JFrame("Tic Tac Toe");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-        JPanel panel = new JPanel(); //creating a panel with a box like a tic tac toe board
-        panel.setLayout(new GridLayout(3, 3));
-        panel.setBorder(BorderFactory.createLineBorder(Color.gray, 10));
-        panel.setBackground(Color.white);
-
-        for (int i = 0; i <= 8; i++) { //placing the button onto the board
-            buttons[i] = new MyButton();
-            panel.add(buttons[i]);
-        }
-
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setSize(500, 500);// set frame size and let teh game begin
-    }
-
-    public static int xOrO = 0; // used for counting
-
-    private static class MyButton extends JButton
-            implements ActionListener {//creating own button class because JButton sucks:)
-
-        int again = 1000;//set again at 1000 so we don't make the mistake we can play again
-        boolean win = false; // there is not a win
-        String letter; // x or o
-
-        public MyButton() {    // creating blank board
-            super();
-            letter = " ";
-            setFont(new Font("Dialog", 1, 60));
-            setText(letter);
-            addActionListener(this);
-        }
-
-        public void actionPerformed(ActionEvent e) { // placing x or o's
-            if ((xOrO % 2) == 0 && getText().equals(" ") && win == false) {
-                letter = "X";
-                xOrO = xOrO + 1;
-                System.out.println(letter + "\n" + xOrO);
-            } else if ((xOrO % 2) == 1 && getText().equals(" ") && win == false) {
-                letter = "O";
-                xOrO = xOrO + 1;
-                System.out.println(letter + "\n" + xOrO);
-            } // if user does click on a button that is already played, nothing will happen
-
-            setText(letter); // place the x or the o on the actual board
-
-
-            for (int i = 0; i <= 7; i++) { // check for the winning combinations
-                if (buttons[winCombinations[i][0]].getText().equals(buttons[winCombinations[i][1]].getText()) &&
-                        buttons[winCombinations[i][1]].getText().equals(buttons[winCombinations[i][2]].getText()) &&
-                        buttons[winCombinations[i][0]].getText() != " ") {//the winning is true
-                    win = true;
-                }
-            }
-
-            if (win == true) { // if the game ends let the user know who wins and give option to play again
-                again = JOptionPane.showConfirmDialog(null, letter + " wins the game!  Do you want to play again?", letter + "won!", JOptionPane.YES_NO_OPTION);
-
-            } else if (xOrO == 9 && win == false) {//tie game, announce and ask if the user want to play again
-                again = JOptionPane.showConfirmDialog(null, "The game was tie!  Do you want to play again?", "Tie game!", JOptionPane.YES_NO_OPTION);
-                win = true;
-            }
-
-            if (again == JOptionPane.YES_OPTION && win == true) { // if the user want to play again clear all the button and start over
-                clearButtons();
-                win = false;
-            } else if (again == JOptionPane.NO_OPTION) {
-                System.exit(0); // exit game if the user do not want to play again
-            }
-
-
-        }
-
-    }
-
-    public static void clearButtons() {
-
-        for (int i = 0; i <= 8; i++) {// clear all 8 buttons
-            buttons[i].setText(" ");
-        }
-        xOrO = 0; // reset the count
-    */
-
-
